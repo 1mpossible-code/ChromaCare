122949c8894fbad38cbb420b787bab81f2718a05
Scriptize <87991619+Scriptize@users.noreply.github.com>
2023-02-18 19:40:36 -0500

    added handling for edge case

--- app/Services/ColorConversion.py
+++ app/Services/ColorConversion.py
@@ -5,37 +5,42 @@ This module provides a service to convert hex and rdga color codes.
 
 class ColorConversion:
     """
-    ColorConversion converts hex and rgba codes.
+    ColorConversion coverts hex and rgba codes.
     """
 
     @staticmethod
     def convert(color):
         """
-        Converts color code (hex,rgba) to rgb
-        :param color: color code (hex, rgba)
+        Converts color code (hex, rgba, rgb) to rgb
+        :param color: color code (hex, rgba, rgb)
         :type color: str
         :return: rgb code as tuple
         """
         rgb_tuple = None
-    
+
         if color.startswith("#"):
             # Convert hex to rgb
             hex_value = color.lstrip("#")
             if len(hex_value) == 3:
-                hex_value = "".join([c * 2 for c in hex_value])
+                hex_value = "".join([x * 2 for x in hex_value])
             rgb_tuple = tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4))
         elif color.startswith("rgba"):
             # Convert rgba to rgb
             rgba_list = color[5:-1].split(",")
             rgba_tuple = tuple(map(int, rgba_list))
-            
+
             r, g, b, a = rgba_tuple
             r = int((1 - a) * 255 + a * r)
             g = int((1 - a) * 255 + a * g)
             b = int((1 - a) * 255 + a * b)
             rgb_tuple = (r, g, b)
-        
+        elif color.startswith("rgb"):
+            # Convert rgb to tuple
+            rgb_list = color[4:-1].split(",")
+            rgb_tuple = tuple(map(int, rgb_list))
+
         return rgb_tuple
+
 
 converter = ColorConversion()
 
